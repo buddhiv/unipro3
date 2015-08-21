14161ec7a48487cb040753d791795beb2281cd3e
buddhiv <92buddhiv@gmail.com>
2015-08-22 04:25:14 +0530

    Uniployer Login page

--- src/Base/DataAccessBundle/Controller/DefaultController.php
+++ src/Base/DataAccessBundle/Controller/DefaultController.php
@@ -10,4 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('BaseDataAccessBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function homeAction()
+    {
+        return $this->render('BaseDataAccessBundle:Default:home.html.twig');
+    }
+
+    public function enterJobAction()
+    {
+        return $this->render('BaseDataAccessBundle:JobViews:enter_job.html.twig');
+    }
 }
